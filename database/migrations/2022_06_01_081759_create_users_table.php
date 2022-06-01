0ca36855897cdf5925df6af71c904015b355f271
oxychan <anwrtaufik@gmail.com>
2022-06-01 23:20:00 +0700

    fixing error

--- database/migrations/2022_06_01_081759_create_users_table.php
+++ database/migrations/2022_06_01_081759_create_users_table.php
@@ -18,8 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name', 50);
             $table->string('email', 50)->unique();
             $table->unsignedBigInteger('role_id');
-            $table->timestamp('email_verified_at');
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 50);
+            $table->rememberToken();
             $table->string('profile_path', 50);
             $table->foreign('role_id')->references('id')->on('roles');
             $table->timestamps();
