8bf6bb7646404da0481c8b9416cdce299fca087a
Atmayanti <mayablue74@gmail.com>
2022-06-15 09:45:01 +0700

    change user and role in frontend navbar and sidebar

--- resources/views/layouts/partials/sidebarMenu.blade.php
+++ resources/views/layouts/partials/sidebarMenu.blade.php
@@ -8,8 +8,8 @@
           <!--change to offline or busy as needed-->
         </div>
         <div class="nav-profile-text d-flex flex-column">
-          <span class="font-weight-bold mb-2">Taufik Anwar</span>
-          <span class="text-secondary text-small">Staff Dapur</span>
+          <span class="font-weight-bold mb-2">{{auth()->user()->name }}</span>
+          <span class="text-secondary text-small">{{auth()->user()->role->role_name }}</span>
         </div>
         <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
       </a>
--- resources/views/layouts/partials/sidebarPayment.blade.php
+++ resources/views/layouts/partials/sidebarPayment.blade.php
@@ -8,8 +8,8 @@
           <!--change to offline or busy as needed-->
         </div>
         <div class="nav-profile-text d-flex flex-column">
-          <span class="font-weight-bold mb-2">Atmayanti</span>
-          <span class="text-secondary text-small">Kasir</span>
+          <span class="font-weight-bold mb-2">{{auth()->user()->name }}</span>
+          <span class="text-secondary text-small">{{auth()->user()->role->role_name }}</span>
         </div>
         <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
       </a>
